8dd6596e13d8248cc63eb27389aaaf508059acc5
Cooking With Pwn Juice <pwnjuicethechef@gmail.com>
2025-07-13 01:15:58 +0000

    making progress?

--- workspace/src/lib/firebase.ts
+++ workspace/src/lib/firebase.ts
@@ -7,9 +7,9 @@ import { getFirestore } from 'firebase/firestore';
 // Find these values in your Firebase project settings under "General".
 const firebaseConfig = {
   apiKey: "AIzaSyDC5PpahjVXu4L8GPb9C04k0k78hq5IVkk",
-  authDomain: "transcript-studio-4drhv.firebaseapp.com",
+  authDomain: "transcript-studio-4drhv.appspot.com",
   projectId: "transcript-studio-4drhv",
-  storageBucket: "transcript-studio-4drhv.firebasestorage.app",
+  storageBucket: "transcript-studio-4drhv.appspot.com",
   messagingSenderId: "371403164462",
   appId: "1:371403164462:web:1bac6e64e9f8e48d8308bf"
 };
